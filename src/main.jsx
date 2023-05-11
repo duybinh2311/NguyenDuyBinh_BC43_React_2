f3b77638431124337c135dc52fbd0f116c67c708
duybinh2311 <duybinh.nguyen2311@gmail.com>
2023-05-11 23:31:38 +0700

    update alias

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GlassesApp from './Components/GlassesApp/GlassesApp'
+import GlassesApp from 'components/GlassesApp/GlassesApp'
+// import GlassesApp from '@/Components/GlassesApp/GlassesApp'
+import second from '@/Components/GlassesApp/GlassesApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
